3309a9b9087cd3209cc969e3dbd394b3a96df3a3
Niels Asp Fuglsang <nielsaspfuglsang@gmail.com>
2021-04-21 14:29:37 +0200

    Added linescan downsampling

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import torch
 import torch.nn as nn
 
@@ -7,3 +9,18 @@ def orthogonal_init(module, gain=nn.init.calculate_gain('relu')):
         nn.init.orthogonal_(module.weight.data, gain)
         nn.init.constant_(module.bias.data, 0)
     return module
+
+
+def down_sample(im, stride=4):
+    """Downsampling corresponding to reducing number of linescans in x- and y-direction."""
+    mask = np.zeros(im.shape)
+    m, n, _ = im.shape
+    mask_x = np.arange(0, n, stride)
+    mask_y = np.arange(0, m, stride)
+    mask[:, mask_x] = 1
+    mask[mask_y, :] = 1
+    
+    # Information kept.
+    # print(np.mean(mask))
+    
+    return im * mask

cd7b18e978b23ce7cdf6b0c83820c6372600f53a
Mads Emil <madsemildh@gmail.com>
2021-04-23 20:20:35 +0200

    Added unet

--- src/utils.py
+++ src/utils.py
@@ -16,11 +16,11 @@ def orthogonal_init(module, gain=nn.init.calculate_gain('relu')):
 def down_sample(im, stride=4):
     """Downsampling corresponding to reducing number of linescans in x- and y-direction."""
     mask = np.zeros(im.shape)
-    m, n, _ = im.shape
+    _, m, n = im.shape
     mask_x = np.arange(0, n, stride)
     mask_y = np.arange(0, m, stride)
-    mask[:, mask_x] = 1
-    mask[mask_y, :] = 1
+    mask[:, :, mask_x] = 1
+    mask[:, mask_y, :] = 1
         
     return (im * mask).type(torch.float32)
 

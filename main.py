80d3b1c532df7c41fe083d8782abb30354a73e44
Niels Asp Fuglsang <nielsaspfuglsang@gmail.com>
2021-04-28 15:27:54 +0200

    Updated main script

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from tqdm import tqdm
-
 import matplotlib.pyplot as plt
 import numpy as np
 import skimage.io
@@ -30,12 +28,12 @@ train_size = 10
 test_size = 15-train_size
 
 print("Downsampling and interpolating...")
-for i, im in tqdm(enumerate(ims_gt)):
+for i, im in enumerate(ims_gt):
     # Downsample.
     im_ds, mask = down_sample(im, stride=12, is_torch=False)
     ims_ds.append(im_ds)
 
-    mask2d = (1 - mask[:,:,0]).astype(np.bool)
+    mask2d = (1 - mask[:,:,0]).astype(bool)
     # Interpolate.
     im_interp = interpolate_rgb(im_ds, mask2d)
     ims_interp.append(im_interp)
@@ -66,25 +64,23 @@ unet = MyUNet()
 unet.to(device)
 
 # Train model.
-n_epochs = 10
-val_size = 1
+n_epochs = 50
+val_size = 5
 
 mse_loss = nn.MSELoss()
 optimizer = optim.AdamW(unet.parameters(), lr=0.001)
-train_loss = []
-test_loss = []
+train_losses = []
+test_losses = []
 
 for epoch in range(n_epochs):
     # Training
     unet.train()
-    batch_loss = []
     for X, y in zip(Xtrain, ytrain):
         X = X.to(device)
         y = y.to(device)
         optimizer.zero_grad()
         pred = unet(X[None, :])
         loss = mse_loss(pred, y[None, :])
-        batch_loss.append(loss.item())
         loss.backward()
         optimizer.step()
     
@@ -92,27 +88,66 @@ for epoch in range(n_epochs):
     if epoch % val_size == 0:
         unet.eval()
         
-        train_loss.append(np.mean(batch_loss))
-        
         with torch.no_grad():
-            batch_loss = []
+            """
+            train_pred = unet(Xtrain.to(device))
+            train_loss = mse_loss(train_pred, ytrain.to(device))
+            train_losses.append(train_loss)
+
+            test_pred = unet(Xtest.to(device))
+            test_loss = mse_loss(test_pred, ytest.to(device))
+            test_losses.append(test_loss)
+            """
+            train_loss = []
+            for X, y in zip(Xtrain, ytrain):
+                X = X.to(device)
+                y = y.to(device)
+                pred = unet(X[None, :])
+                loss = mse_loss(pred, y[None, :])
+                train_loss.append(loss.item())
+            train_losses.append(np.mean(train_loss))
+
+            test_loss = []
             for X, y in zip(Xtest, ytest):
                 X = X.to(device)
                 y = y.to(device)
                 pred = unet(X[None, :])
                 loss = mse_loss(pred, y[None, :])
-                batch_loss.append(loss.item())
+                test_loss.append(loss.item())
+            test_losses.append(np.mean(test_loss))
 
-            test_loss.append(np.mean(batch_loss))
 
-        print(f"epoch: {epoch} \t train loss: {train_loss[-1]:.6f} \t test loss {test_loss[-1]:.6f}")
-    
+        print(f"epoch: {epoch} \t train loss: {train_losses[-1]:.6f} \t test loss {test_losses[-1]:.6f}")
 
+
+fig, ax = plt.subplots(1, 1, figsize=(10,5))
+ax.plot(np.arange(0, n_epochs, val_size), train_losses, linewidth=3, label='train error')
+ax.plot(np.arange(0, n_epochs, val_size), test_losses, linewidth=3, label='test error')
+ax.set_yscale('log')
+ax.set_ylabel('MSE')
+ax.legend()
+plt.savefig('convergence.png')
+plt.show()
+
+
+# Get reconstructed data in numpy.
+unet.eval()
+ims_superres = np.zeros(ims_gt.shape)
+for i in range(15):
+    with torch.no_grad():
+        X = interp_data[None, i].to(device)
+        pred = unet(X)
+        pred_np = pred[0].cpu().detach().numpy().transpose((1,2,0))
+        pred_np = np.clip(pred_np, 0, 1)
+        ims_superres[i] = pred_np
+
+# Get MSE and PSNR 
 unet_mse_list = []
 unet_psnr_list = []
-for i, im in enumrate(ims_interp[train_size:]):
-    unet_mse_list.append(mse(im_interp, im))
-    unet_psnr_list.append(psnr(im_interp, im))
+
+for i in range(train_size, 15):
+    unet_mse_list.append(mse(ims_superres[i], ims_gt[i]))
+    unet_psnr_list.append(psnr(ims_superres[i], ims_gt[i]))
 
 print('Mean MSE of interpolation:', np.mean(interp_mse_list))
 print('Mean PSNR of interpolation:', np.mean(interp_psnr_list))
@@ -120,14 +155,18 @@ print('Mean MSE of U-net:', np.mean(unet_mse_list))
 print('Mean MSE of U-net:', np.mean(unet_psnr_list))
 
 
+
+# Plot reconstruction success.
 idx = 14
 
 plt.rcParams.update({'font.size': 22})
-fig, axes = plt.subplots(1, 2, figsize=(40, 20))
+fig, axes = plt.subplots(1, 3, figsize=(40, 20))
 
 axes[0].imshow(ims_gt[idx])
 axes[0].set_title('Original')
 axes[1].imshow(ims_interp[idx])
 axes[1].set_title('Interpolated')
+axes[2].imshow(ims_superres[idx])
+axes[2].set_title('U-Net')
 plt.savefig('super-duper{}.png'.format(idx))
 plt.show()
